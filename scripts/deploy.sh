2f08939e637b1722f830722e9d83096a24010c3e
Nikita Galayko <ngalayko@gmail.com>
2018-10-13 10:25:32 +0200

    CI: more logs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+echo "Deploying..."
+
 files=""
 for file in $(ls *.yaml); do
     files="${files} -f ${file} "
 done
 
 docker-compose ${files} up --build -d --remove-orphans
+
+echo "Done."
