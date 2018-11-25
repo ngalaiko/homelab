7b5d28445aaf7e2245bc01f108f34f847a4c4e68
Nikita Galayko <ngalayko@gmail.com>
2018-11-25 22:01:17 +0100

    filter access log [2]

--- containers/goaccess/entrypoint.sh
+++ containers/goaccess/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-tail -f -n +0 /srv/log/access.log \
-    | grep --line-buffered 'Host-galayko-rocks' > /srv/log/access.filtered.log
+tail -f -n +0 /srv/log/traefik/access.log \
+    | grep --line-buffered 'Host-galayko-rocks' > /srv/log/traefik/access.filtered.log
 
 /sbin/tini --  goaccess \
     --no-global-config \
