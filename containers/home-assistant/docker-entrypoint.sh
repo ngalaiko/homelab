7d129ce93e9037dec9555b0b755a277c4997e9dd
Nikita Galayko <ngalayko@gmail.com>
2018-12-26 20:25:53 +0100

    secrets management from env for home assistant

---
+++ containers/home-assistant/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CONFIG_FILES=(
+    "automations.yaml"
+    "configuration.yaml"
+    "customize.yaml"
+    "groups.yaml"
+    "scripts.yaml"
+    "secrets.yaml"
+)
+
+function fill_from_env() {
+    file_name="$1"
+
+    if [ ! -f ${file_name} ]; then
+    	echo "File ${file_name} not found!"
+		return
+	fi
+
+    for name_value in $(env); do
+        name="${name_value%=*}"
+        value="${name_value#*=}"
+
+        sed -i -e "s/\${${name}}/${value}/g" ${file_name}
+    done
+}
+
+for file in ${CONFIG_FILES[@]}; do
+    fill_from_env "${file}"
+done
+
+#python -m homeassistant --config /config
