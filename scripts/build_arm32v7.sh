9141d9b85aadce36d5aa3510813bbca3c6121323
Nikita Galayko <ngalayko@gmail.com>
2018-12-29 13:38:16 +0100

    disable wifi

--- scripts/build_arm32v7.sh
+++ scripts/build_arm32v7.sh
@@ -6,10 +6,10 @@ echo "Building images..."
 
 # NOTE: ./server is the root
 BUILD_IMAGES=(
+    "./containers/docker-ap:ngalayko/docker-ap"
     "./containers/miniflux:ngalayko/miniflux"
     "./containers/traefik:ngalayko/proxy"
     "./containers/home-assistant:ngalayko/home-assistant"
-    "./containers/docker-ap:ngalayko/docker-ap"
     "./containers/blog:ngalayko/blog"
     "./containers/fathom:ngalayko/fathom"
     "./containers/postgres:ngalayko/postgres"
