458dca900a74f31afe8adcb9da11aaa53b8e40f8
Nikita Galaiko <nikita@galaiko.rocks>
2019-06-26 22:22:56 +0200

    init secrets deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,11 +7,11 @@ chmod 600 .travis/deploy_rsa # Allow read access to the private key
 ssh-add .travis/deploy_rsa # Add the private key to SSH
 
 # move ENV_* variables to the remote server
-# NOTE: they are moved without ENV_ prefix
+# NOTE: they are moved with ENV_ prefix
 VARS="echo env variables updated"
 for v in $(env); do
     if [[ "${v}" =~ ^ENV_* ]]; then
-        VARS="${VARS} && export ${v/ENV_/}"
+        VARS="${VARS} && export ${v}"
     fi
 done
 
@@ -23,5 +23,5 @@ ssh -i .travis/deploy_rsa $USER@$IP <<EOF
 
     git pull --force
 
-    ./scripts/deploy_arm32v7.sh
+    ./scripts/k8s/apply.sh
 EOF
--- scripts/k8s/apply.sh
+++ scripts/k8s/apply.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+for v in $(env); do
+    if [[ "${v}" =~ ^ENV_* ]]; then
+        VARS="${VARS} && export ${v}"
+        echo $v
+    fi
+done
+
 files=""
 for file in $(find ./k8s -name '*.yaml'); do
     files="${files} -f ${file} "
